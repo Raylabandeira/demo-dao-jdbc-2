5ed4a44cb4b57c0a671496b617410f6db36d04fb
Rayla <raylafortunato@gmail.com>
2025-12-27 20:38:16 -0300

    DepartmentDao implementation and test

---
+++ src/model/dao/DepartmentDaoJDBC.java
@@ -0,0 +1,44 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	public DepartmentDaoJDBC(Connection connection) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
